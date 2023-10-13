40d0de407664e20d7bbd6b25719e61ecc04e2692
Catasan GG <catasangg@gmail.com>
2023-10-13 19:34:19 +0300

    Refactor

--- src/main/java/com/example/NewsService/web/controller/NewsController.java
+++ src/main/java/com/example/NewsService/web/controller/NewsController.java
@@ -4,14 +4,18 @@ import com.example.NewsService.model.News;
 import com.example.NewsService.service.KafkaDataService;
 import com.example.NewsService.web.dto.NewsDto;
 import com.example.NewsService.web.mapper.DataMapper;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.validation.Valid;
+import jakarta.validation.ConstraintViolation;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/news")
@@ -28,5 +32,15 @@ public class NewsController {
         News news = dataMapper.toEntity(newsDto);
         kafkaDataService.send(news);
 
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
+        String errorMessage = ex.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("; "));
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/NewsService/web/dto/NewsDto.java
+++ src/main/java/com/example/NewsService/web/dto/NewsDto.java
@@ -10,8 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class NewsDto {
-    @NotEmpty(message = "company should be not empty")
+    @NotEmpty(message = "company_tag should not be empty")
     private String company_tag;
-    @NotEmpty(message = "content should be not empty")
+    @NotEmpty(message = "Content should not be empty")
     private String content;
 }
